147cb2dc38196e42bf9c0e4c2287dde4d7bc7019
Naksuuuu <ariel.233040088@mail.unpas.ac.id>
2024-10-09 21:35:17 +0700

    pbd latihan1

---
+++ insertDataMhs.sql
@@ -0,0 +1,8 @@
+INSERT INTO dbo.mhs (id, NIM, Nama, Jurusan, TanggalLahir)
+VALUES 
+(1,'233040088', 'ariel', 'teknik', '2004-12-17'),
+(2,'233040089', 'wijaya', 'teknik', '2004-12-17'),
+(3,'233040090', 'kusuma', 'teknik', '2004-12-17'),
+(4,'233040091', 'l', 'teknik', '2004-12-17'),
+(5,'233040092', 'b', 'teknik', '2004-12-17'),
+(6,'233040094', 'c', 'teknik', '2004-12-17')
