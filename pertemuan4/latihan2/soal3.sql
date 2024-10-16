62becb3e176dab9ddf6de5fbdc4acf8cdea7356b
Naksuuuu <ariel.233040088@mail.unpas.ac.id>
2024-10-16 12:10:25 +0700

    pertemuan 4 & latihan

---
+++ pertemuan4/latihan2/soal3.sql
@@ -0,0 +1,3 @@
+SELECT  COUNT(*) as jumlah_total_mahasiswa_informatika
+FROM mahasiswa
+WHERE Jurusan = 'Teknik Informatika'
---
+++ pertemuan4/latihan2/soal6.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(*) as Matkul_hari_senin
+FROM jadwal_mata_kuliah 
+WHERE hari = 'senin'
